f3deddd8f35dd4dc2ee7dbb565255dd9ba238abd
Sultepronit <sultepronit@gmail.com>
2022-09-30 11:12:45 +0300

    hello world!

--- Web/SpringBootHelloWorld/lib/src/main/java/app/App.java
+++ Web/SpringBootHelloWorld/lib/src/main/java/app/App.java
@@ -3,7 +3,7 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"main"})
 public class App {
 
 	public static void main(String[] args) {
