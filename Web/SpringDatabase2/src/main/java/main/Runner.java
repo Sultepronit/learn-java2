b2b9e4e789ce822da93ad80d5b4c06353950008d
Sultepronit <sultepronit@gmail.com>
2022-10-03 19:40:38 +0300

    added functions of retrieving data from database

--- Web/SpringDatabase2/src/main/java/main/Runner.java
+++ Web/SpringDatabase2/src/main/java/main/Runner.java
@@ -17,9 +17,17 @@ public class Runner implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		
 		System.out.println("Saving...");
-		var user1 = new User("Mihal", "mihal@com.ua");
-		userDao.save(user1);
+		var user1 = new User("Charles", "charles@com.ua");
+		var user0 = userDao.save(user1);
+		System.out.println(user0);
 		
+		userDao.findAll().forEach(u -> System.out.println("Find all: " + u));
+		
+		var retrievedUserOpt = userDao.findById(1l);
+		
+		if(retrievedUserOpt.isPresent()) {
+			System.out.println("Find by id: " + retrievedUserOpt.get());
+		}
 	}
 	
 }
