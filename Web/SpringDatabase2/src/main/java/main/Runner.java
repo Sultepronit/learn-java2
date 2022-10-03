31c55e9bb7ff7332eeeafd5076b6d7497d8f2be9
Sultepronit <sultepronit@gmail.com>
2022-10-03 13:20:45 +0300

    added function of saving entries to table

--- Web/SpringDatabase2/src/main/java/main/Runner.java
+++ Web/SpringDatabase2/src/main/java/main/Runner.java
@@ -4,19 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import greeters.Greeter;
+import entities.User;
+import repositories.UserDao;
 
 @Component
 public class Runner implements CommandLineRunner {
 
 	@Autowired
-	private Greeter greeter; 
+	private UserDao userDao;
 	
 	@Override
 	public void run(String... args) throws Exception {
 		
-		System.out.println("And greet!");
-		greeter.greet();
+		System.out.println("Saving...");
+		var user1 = new User("Mihal", "mihal@com.ua");
+		userDao.save(user1);
 		
 	}
 	
