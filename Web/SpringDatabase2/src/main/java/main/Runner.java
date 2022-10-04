e4fbb2843fe9765da5ebf2ca9e436d92352ac267
Sultepronit <sultepronit@gmail.com>
2022-10-04 13:47:00 +0300

    added CrudRepository method

--- Web/SpringDatabase2/src/main/java/main/Runner.java
+++ Web/SpringDatabase2/src/main/java/main/Runner.java
@@ -16,18 +16,24 @@ public class Runner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		System.out.println("Saving...");
+		/*System.out.println("Saving...");
 		var user1 = new User("Charles", "charles@com.ua");
 		var user0 = userDao.save(user1);
 		System.out.println(user0);
+		*/
 		
+		//default method
 		userDao.findAll().forEach(u -> System.out.println("Find all: " + u));
 		
+		//default method
 		var retrievedUserOpt = userDao.findById(1l);
-		
 		if(retrievedUserOpt.isPresent()) {
 			System.out.println("Find by id: " + retrievedUserOpt.get());
 		}
+		
+		//method that has to be mentioned in UserDao interface
+		var users = userDao.findByName("Charles");
+		users.forEach(u -> System.out.println("Find by name: " + u));
 	}
 	
 }
