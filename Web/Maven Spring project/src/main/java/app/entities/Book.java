cc8e1798eefb6863e148de3154f87ad11f5f20ff
Sultepronit <sultepronit@gmail.com>
2022-10-08 10:25:39 +0300

    connected to database and created table

--- Web/Maven Spring project/src/main/java/app/entities/Book.java
+++ Web/Maven Spring project/src/main/java/app/entities/Book.java
@@ -2,9 +2,24 @@ package app.entities;
 
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="books")
 public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(length = 100, nullable = false)
 	private String title;
+	
+	@Column(length = 50, nullable = false)
 	private String author;
 
 	public Book() {
