166859e0f4aed6d2ef439d896502c48a30098916
Sultepronit <sultepronit@gmail.com>
2022-10-10 19:08:15 +0300

    added a split pane

--- Web/JSON client (last)/src/main/java/app/controllers/Controller.java
+++ Web/JSON client (last)/src/main/java/app/controllers/Controller.java
@@ -1,12 +1,19 @@
 package app.controllers;
 
+import gui.CreateBookPanel;
 import gui.MainFrame;
+import gui.ViewBooksPanel;
 
 public class Controller {
 	
 	private MainFrame mainFrame;
+	private CreateBookPanel createPanel;
+	private ViewBooksPanel viewPanel;
 	
 	public Controller() {
-		mainFrame = new MainFrame();
+		createPanel = new CreateBookPanel();
+		viewPanel = new ViewBooksPanel();
+		
+		mainFrame = new MainFrame(createPanel, viewPanel);
 	}
 }
