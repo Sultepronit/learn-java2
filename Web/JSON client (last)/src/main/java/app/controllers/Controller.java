8dacb273d66a5d28e3495c375eb2de4ce5e183da
Sultepronit <sultepronit@gmail.com>
2022-10-12 21:50:45 +0300

    connected to the web server

--- Web/JSON client (last)/src/main/java/app/controllers/Controller.java
+++ Web/JSON client (last)/src/main/java/app/controllers/Controller.java
@@ -1,18 +1,29 @@
 package app.controllers;
 
+import java.io.IOException;
+
 import app.gui.CreateBookPanel;
 import app.gui.MainFrame;
 import app.gui.ViewBooksPanel;
+import services.BookService;
 
 public class Controller {
 	
 	private MainFrame mainFrame;
 	private CreateBookPanel createPanel;
 	private ViewBooksPanel viewPanel;
+	private BookService bookService;
 	
 	public Controller() {
-		createPanel = new CreateBookPanel();
+		bookService = new BookService();
+		try {
+			bookService.getAll();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		createPanel = new CreateBookPanel();
 		createPanel.setFormListener((author, title) -> {
 			System.out.println(author + ": " + title);
 		});
