3027224b3370a8a6e67adbeeabca25bc6cb8a125
Sultepronit <sultepronit@gmail.com>
2022-10-28 12:36:53 +0300

    added a module & some little changes

--- Practice/sandbox/src/com/sandbox/HelloWorld.java
+++ Practice/sandbox/src/com/sandbox/HelloWorld.java
@@ -3,5 +3,6 @@ package com.sandbox;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, IDEA");
+        System.out.println(args[0]);
     }
 }
